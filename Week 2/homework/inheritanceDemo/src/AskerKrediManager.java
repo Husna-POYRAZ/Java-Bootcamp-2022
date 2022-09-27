de3159cb93d35b5bf437c761ce0dee81c812cb9a
Hüsna POYRAZ <75911392+Husna-POYRAZ@users.noreply.github.com>
2022-09-27 23:14:33 +0300

    homework

---
+++ Week 2/homework/inheritanceDemo/src/AskerKrediManager.java
@@ -0,0 +1,9 @@
+public class AskerKrediManager extends BaseKrediManager {
+    public AskerKrediManager() {
+        System.out.println("Asker kredi manager çalıştı.");
+    }
+
+    public void hesapla() {
+        System.out.println("Asker kredisi hesaplandı.");
+    }
+}
