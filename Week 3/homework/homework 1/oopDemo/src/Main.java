39d61edc3368c3ee591e60b3d70e327d7252c07c
Hüsna POYRAZ <75911392+Husna-POYRAZ@users.noreply.github.com>
2022-10-11 00:13:28 +0300

    homework

--- Week 3/homework/homework 1/oopDemo/src/Main.java
+++ Week 3/homework/homework 1/oopDemo/src/Main.java
@@ -1,14 +1,36 @@
 public class Main {
     public static void main(String[] args) {
-        CreditManager creditManager = new CreditManager();
-        creditManager.calculate();
-        creditManager.save();
+//		CreditManager creditManager = new CreditManager();
+//		creditManager.calculate();
+//		creditManager.calculate();
+//		creditManager.save();
+//
+//		Customer customer = new Customer(); //örneğini oluşturmak,instance oluşturmak,instance creation
+//		customer.id = 1;
+//		customer.city = "Ankara";
+//
+//		CustomerManager customerManager = new CustomerManager(customer);
+//		customerManager.save();
+//		customerManager.delete();
+//
+//		Company company = new Company();
+//		company.taxNumber="100000";
+//		company.companyName= "Arçelik";
+//		company.Id=100;
+//
+//		CustomerManager customerManager2 = new CustomerManager(new Person());
+//
+//		Person person = new Person();
+//		person.firstName = "Hüsna";
+// 		person.lastName = "POYRAZ";
+//		person.NationalIdentity= "12*****12";
+//
+//		Customer c1 = new Customer();
+//		Customer c2 = new Person();
+//		Customer c3 = new Company();
 
-        Customer customer = new Customer();
-        customer.setId(1);
-        customer.setCity("Ankara");
 
-        CustomerManager customerManager = new CustomerManager(new Customer(), new MilitaryCreditManager());
+        CustomerManager customerManager = new CustomerManager(new Customer(),new TeacherCreditManager());
         customerManager.giveCredit();
     }
 }
